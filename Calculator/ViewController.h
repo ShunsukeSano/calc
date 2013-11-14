e1c63e74fc7633eece539609144829eee903acd1
ShunsukeSano <s1531029@eng.kitakyu-u.ac.jp>
2013-11-14 15:57:06 +0900

    入力受付　クリア

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -10,4 +10,23 @@
 
 @interface ViewController : UIViewController
 
+@property NSUInteger isInitialState;//初期状態かどうか　YES：初期　NO：違う
+
+@property NSInteger transrateValue;//文字列を数値に変換したものを格納する変数
+
+@property NSInteger currentValue;//演算子が押されたときに現在の数値を格納する。
+
+@property NSInteger operation;//どの演算子が押されたかの判別
+
+
+- (IBAction)numberButtom:(id)sender;
+
+- (IBAction)operationButtom:(id)sender;
+
+- (IBAction)clearButtom:(id)sender;
+
+- (IBAction)equalButtom:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *Result;
+
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -14,16 +14,68 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.isInitialState = YES;
+    	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+- (IBAction)numberButtom:(id)sender {
+    UIButton *b = (UIButton *)sender;
+        if(self.isInitialState){
+            if(b.tag != 0){
+                _Result.text = [NSString
+                                stringWithFormat:@"%d", b.tag];
+                self.isInitialState = NO;
+            }
+        } else {
+            _Result.text = [NSString
+                            stringWithFormat:@"%@%d", _Result.text,b.tag];
+            self.transrateValue = _Result.text.integerValue;
+        }
+}
+
+
+
+
+
+
+
+- (IBAction)operationButtom:(id)sender {
+        UIButton *b = (UIButton *)sender;
+        self.currentValue = self.transrateValue;
+        self.operation = b.tag;
+}
+
+
+
+
+
+
+
+- (IBAction)clearButtom:(id)sender {
+    _Result.text = @"0";
+    self.isInitialState = YES;
+    
+}
+
+
+
+
+
+
+
+- (IBAction)equalButtom:(id)sender {
 }
 
 @end
